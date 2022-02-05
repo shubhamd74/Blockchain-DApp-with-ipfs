be87ebb7acd0a70b51eb204b844a0fb3653aa450
Shubham Deshmukh <shubhamdeshmukh9011@gmail.com>
2022-02-05 21:32:58 +0530

    first commit

---
+++ client/src/ipfs.js
@@ -0,0 +1,3 @@
+const IPFS = required('ipfs-api');
+const ipfs = new IPFS({host: 'ipfs.infura.io',port:5001,protocol: 'https'});
+export default ipfs;
---
+++ truffle-config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  networks:{
+    developement: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // match any network id
+    }
+  }
+};
